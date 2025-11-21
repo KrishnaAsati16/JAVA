42d2aed3ace599d34b1ab2cb71ebfe3148053b28
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-11-21 21:13:11 +0530

    class and object Q.

---
+++ class_object.java
@@ -0,0 +1,42 @@
+// public class class_object {
+//     public static void main(String[] args) {
+        
+//         class Student {
+//     // fields
+//     String name;
+//     int rollNumber;
+//     double marks;
+
+//     // constructor
+//     Student(String name, int rollNumber, double marks) {
+//         this.name = name;
+//         this.rollNumber = rollNumber;
+//         this.marks = marks;
+//     }
+
+//     // method to display student details
+//     void display() {
+//         System.out.println("Name : " + name);
+//         System.out.println("Roll Number : " + rollNumber);
+//         System.out.println("Marks : " + marks);
+//         System.out.println("-----------------------------");
+//     }
+// }
+
+// public class StudentMain {
+//     public static void main(String[] args) {
+//         // creating 3 student objects
+//         Student s1 = new Student("Amam", 1,89.5);
+//         Student s2 = new Student("Rohit", 2, 76.0);
+//         Student s3 = new Student("Neha", 3, 92.3);
+
+//         // displaying student details
+//         s1.display();
+//         s2.display();
+//         s3.display();
+//     }
+// }
+
+//     }
+    
+// }
